57f99faa636104b106a34e5e9c8ee93b437bfbef
Ekaterina Zarubina <zarubina.e4@wb.ru>
2024-09-03 18:08:16 +0700

    feature: добвыила класс для поразрядной сортировки, реализовала счетчик

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,43 @@
 import sort.InsertionSort;
+import sort.RadixSort;
 import sort.Sortable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        int[] array = {12, 23, 13, 6, 5};
-        Sortable insertionSort = new InsertionSort(array);
 
-        System.out.println("Исходный массив: ");
+        List<Integer> arrayNums = new ArrayList<>(Arrays.asList(120, 203, 130, 600, 500, 908, 456, 101, 999, 100, 107, 707));
+        List<String> arrayStrings = new ArrayList<>(Arrays.asList("рот", "акт", "ярд", "вол"));
 
+        System.out.print("Исходный массив чисел: ");
+        for (int i = 0; i < arrayNums.size(); i++) {
+            System.out.print(arrayNums.get(i) + " ");
+        }
+        System.out.println(" ");
+        System.out.println(" ");
 
-        System.out.println("Массив, отсортированный вставками: ");
-        insertionSort.sortArray(array);
+        //Сортировка вставками
+        System.out.println("СОРТИРОВКА ВСТАВКАМИ");
+        Sortable insertionSort = new InsertionSort(arrayNums);
+        System.out.print("Отсортированный массив: ");
+        insertionSort.sortArray(arrayNums);
+        insertionSort.printArray(arrayNums);
+        System.out.println(" ");
+        System.out.println(" ");
+
+        arrayNums = new ArrayList<>(Arrays.asList(120, 203, 130, 600, 500, 908, 456, 101, 999, 100, 107, 707));
+
+        //Поразрядная сортировка для чисел
+        System.out.println("ПОРАЗРЯДНАЯ СОРТИРОВКА");
+        Sortable radixSort = new RadixSort(arrayNums);
+        System.out.print("Отсортированный массив: ");
+        radixSort.sortArray(arrayNums);
+        radixSort.printArray(arrayNums);
+        System.out.println(" ");
 
 
 
--- src/main/java/sort/Sortable.java
+++ src/main/java/sort/Sortable.java
@@ -1,11 +1,20 @@
 package sort;
 
-public interface Sortable {
-    public void sortArray(int[] array);
+import java.util.List;
 
-    default void defaultPrintArrayMethod(int [] array){
-        for(int i =0; i<array.length; i++){
-            System.out.println(array[i] + ", ");
+public interface Sortable {
+    /*
+    Чтобы метод sortArray работал как для чисел, так и для строк (и любых других объектов, которые можно сравнивать),
+    нужно использовать ограничение на тип T в параметре метода, указав,
+    что T должен реализовывать интерфейс Comparable<T>.
+    Это позволяет сравнивать элементы друг с другом.
+     */
+    <T extends Comparable<T>> void sortArray(List<T> array);
+    //public <T> void sortArray(List<T> array);
+
+    default <T> void printArray(List<T> array){
+        for(int i =0; i<array.size(); i++){
+            System.out.print(array.get(i) + " ");
         }
     }
 }
